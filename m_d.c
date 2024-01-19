04b43d6cc8b10a68ba52d1e06cb49160110a8f96
abderrahmane-elrhassani <abdoghessan@example.com>
2024-01-18 16:25:39 -0800

    Multiplies

---
+++ m_d.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+
+*m_d - Multiplies the second top element of the stack with the top element.
+*@stack: Double pointer to the stack head.
+*@line_number: Script line number.
+*Usage: mod
+*If stack has less than two elements, print error L<line_number>: can't mod,*stack too short
+*and exit with EXIT_FAILURE.
+*Result is stored in second top element, and top element is removed.
+*If top element is 0, print error L<line_number>: division by zero and exit *with EXIT_FAILURE.
+*Return: No return.
+
+*/
+
+void m_d(stack_t **stack, unsigned int line_number)
+{
+	int temp_variable;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp_variable = (*stack)->n;
+	p_p(stack, line_number);
+	if (temp_variable == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->n %= temp_variable;
+}
