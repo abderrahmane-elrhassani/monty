cb6564bdba4fcd9b11c21129d957d228af2cd413
abderrahmane-elrhassani <abdoghessan@example.com>
2024-01-18 17:13:56 -0800

    doing nothing

---
+++ n_p.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+
+*n_p - Function that does nothing.
+*@stack: Double pointer to the stack head.
+*@line_number: Script line number.
+*Usage: nop
+*Return: No return.
+
+*/
+
+void n_p(stack_t **stack, unsigned int line_number)
+{
+	(void)(stack);
+	(void)(line_number);
+}
