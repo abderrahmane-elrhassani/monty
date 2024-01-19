94af9b925894ba4befe5007457f06a0b2faf1fe5
abderrahmane-elrhassani <abdoghessan@example.com>
2024-01-18 18:26:59 -0800

    main

---
+++ main.c
@@ -0,0 +1,107 @@
+#include "monty.h"
+
+glob_vars globv;
+
+
+/**
+ * main - Entry point for Monty bytecode interpreter.
+ * @ac: Number of command line arguments.
+ * @av: Pointer to array containing command line arguments.
+ *
+ * Usage: monty file
+ * If no file or more than one argument is given, print error USAGE: monty file
+ * and exit with EXIT_FAILURE.
+ * If unable to open the file, print error Error: Can't open file <file>
+ * and exit with EXIT_FAILURE where <file> is the name of the file.
+ * If the file contains an invalid instruction, print error L<line_number>: unknown instruction <opcode>
+ * and exit with EXIT_FAILURE, where <line_number> is the line number where the instruction appears.
+ * If malloc fails, print error Error: malloc failed and exit with EXIT_FAILURE.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	stack_t *head;
+
+	s_i(&head);
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	f_r(av[1], &head);
+	exit(EXIT_SUCCESS);
+}
+/**
+ * f_r - Processes the entire Monty opcode file.
+ * @filename: String name of the Monty opcode file.
+ * @stack: Double pointer to the top of the stack data structure.
+ * Return: Returns an error code or success.
+ **/
+
+int f_r(char *filename, stack_t **stack)
+{
+	size_t len;
+	ssize_t read;
+	unsigned int line_number = 0;
+	char *line = NULL;
+	FILE *fp;
+	char *op;
+
+
+	if (!filename)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	atexit(f_a);
+	while ((read = getline(&line, &len, fp)) != -1)
+	{
+		op = strtok(line, "\n\t\r ");
+		line_number++;
+		if (op)
+			got(stack, op, line_number);
+	}
+	free(line);
+	fclose(fp);
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * s_i - Initializes all the necessary elements.
+ * @head: Double pointer to the top of the stack.
+ *
+ * Return: No return.
+ **/
+
+void s_i(stack_t **head)
+{
+	*head = NULL;
+	globv.top = head;
+}
+
+/**
+ * f_a- Frees all dynamically allocated memory.
+ *
+ * Return: No return.
+ **/
+
+void f_a(void)
+{
+	stack_t *tmp1, *tmp2 = NULL;
+
+	tmp1 = *(globv.top);
+	/* printf("glob.top->%p\n",  (void*)glob.top); */
+	while (tmp1 != NULL)
+	{
+		tmp2 = tmp1->next;
+		free(tmp1);
+		tmp1 = tmp2;
+	}
+}
---
+++ monty.h
@@ -0,0 +1,109 @@
+#ifndef MONTY
+#define MONTY
+
+#define  _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * struct stack_s - Doubly linked list representation of a stack (or queue)
+ * @n: Integer
+ * @prev: Points to the previous element of the stack (or queue)
+ * @next: Points to the next element of the stack (or queue)
+ *
+ * Description: Doubly linked list node structure
+ * for stack, queues, LIFO, FIFO in Holberton project.
+ */
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_
+
+/**
+ * struct instruction_s - Opcode and its corresponding function
+ * @opcode: The opcode
+ * @f: Function to handle the opcode
+ *
+ * Description: Opcode and its function
+ * for stack, queues, LIFO, FIFO in Holberton project.
+ */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**
+ * struct global_vars - Globally useful variables rolled into one.
+ * @top: Double pointer to the top of the stack.
+ * @ops: Double pointer to an instruction struct.
+ */
+
+typedef struct global_vars
+{
+	stack_t **top;
+	instruction_t **ops;
+} glob_vars;
+
+extern glob_vars globv;
+
+
+/**
+ * struct var_s - Struct to contain the main variables of the Monty interpreter.
+ * @queue: Flag to determine if in stack vs queue mode.
+ * @stack_len: Length of the sta
+*/
+
+ typedef struct var_s
+{
+	int queue;
+	size_t stack_len;
+} var_t;
+
+var_t var;
+
+extern var_t var;
+
+#define STACK 0
+#define QUEUE 1
+
+void s_i(stack_t **head);
+void f_a(void);
+int f_r(char *filename, stack_t **stack);
+void got(stack_t **stack, char *op, unsigned int line_number);
+void p_h(stack_t **stack, unsigned int line_number);
+void p_l(stack_t **stack, unsigned int line_number);
+int interpreter(char *num_string, unsigned int line_number);
+void p_t(stack_t **stack, unsigned int line_number);
+void p_p(stack_t **stack, unsigned int line_number);
+void s_p(stack_t **stack, unsigned int line_number);
+void a_d(stack_t **stack, unsigned int line_number);
+void n_p(stack_t **stack, unsigned int line_number);
+void s_b(stack_t **stack, unsigned int line_number);
+void d_v(stack_t **stack, unsigned int line_number);
+void m_l(stack_t **stack, unsigned int line_number);
+void m_d(stack_t **stack, unsigned int line_number);
+void pc_r(stack_t **stack, unsigned int line_number);
+void ps_r(stack_t **stack, unsigned int line_number);
+void r_l(stack_t **stack, unsigned int line_number);
+void r_r(stack_t **stack, unsigned int line_number);
+void stak(stack_t **stack, unsigned int line_number);
+void que(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY */
